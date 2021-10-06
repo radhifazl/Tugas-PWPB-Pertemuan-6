b9783255ba37d299c3b20301a32c36c4f707bde9
Radhif Azli <77906566+radhif-zyn@users.noreply.github.com>
2021-10-06 09:43:29 +0700

    Add files via upload

---
+++ Pertemuan 6/Tugas6/Tugas 2 Form/tugas2p6.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="tugas2p6.css">
+
+    <title>Tugas 1 Pertemuan 6</title>
+
+</head>
+<body>
+    <div class="form-container">
+        <form class="form">
+            <h2 id="form-label">Login Inponow</h2>
+
+            <input type="text" name="username" id="username" placeholder="Masukkan Username">
+            <input type="text" name="password" id="password" placeholder="Masukkan Password">
+
+            <div class="button">
+                <button class="submit-btn btn">Submit</button>
+                <button class="reset-btn btn">Reset</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
